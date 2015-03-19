b200b3e63122f432f47b2b4fbc0705854784ebc5
Helithumper <peyton.duncan45@gmail.com>
2015-03-19 08:16:46 -0400

    Wrote Fractal Tree Class

--- src/trees/FractalTree.java
+++ src/trees/FractalTree.java
@@ -1,5 +1,18 @@
 package trees;
 
-public class FractalTree {
+import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.JPanel;
+
+public class FractalTree extends JPanel {
+	
+	
+	public FractalTree(int PANEL_WIDTH, int PANEL_HEIGHT) {
+		setBackground(Color.WHITE);
+		setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+
+	}
+	
+	//Recursive Method
 }
