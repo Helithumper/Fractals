08b478f0f028bd0de2038da5657d7b232a237694
Helithumper <peyton.duncan45@gmail.com>
2015-03-19 09:00:44 -0400

    Colored Large Tree

--- src/trees/FractalTree.java
+++ src/trees/FractalTree.java
@@ -9,20 +9,32 @@ import javax.swing.JPanel;
 public class FractalTree extends JPanel {
 
 	public FractalTree(int PANEL_WIDTH, int PANEL_HEIGHT) {
-		setBackground(Color.WHITE);
+		setBackground(Color.BLUE);
 		setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
-
 	}
 
 	// Recursive Method
+	int i = 0;
+	int j = 0;
+	int k = 0;
 	private void drawTree(Graphics g, int x1, int y1, double angle, int depth) {
 		if (depth == 0) {
 			return;
 		}
-		int x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * depth * 10.0);
-		int y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) * depth * 10.0);
+		i++;
+		j++;
+		k++;
+		int x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * depth * 5.0);
+		int y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) * depth * 5.0);
 		g.drawLine(x1, y1, x2, y2);
-		drawTree(g, x2, y2, angle - 35, depth - 1);
-		drawTree(g, x2, y2, angle + 35, depth - 1);
+		drawTree(g, x2, y2, angle - 15, depth - 1);
+		drawTree(g, x2, y2, angle + 15, depth - 1);
+		g.setColor(new Color(i%255,j%255,k%255,255));
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.setColor(Color.PINK);
+		drawTree(g,200,450,-90,21);
 	}
 }
