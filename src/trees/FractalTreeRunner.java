3a2b0a79e8ee894401c663b0164c8c9c5fee4606
Helithumper <peyton.duncan45@gmail.com>
2015-03-19 08:13:31 -0400

    Wrote Runner Class

---
+++ src/trees/FractalTreeRunner.java
@@ -0,0 +1,21 @@
+package trees;
+
+import javax.swing.JApplet;
+import javax.swing.JPanel;
+
+public class FractalTreeRunner extends JApplet {
+
+	private final int APPLET_WIDTH = 2000;
+	private final int APPLET_HEIGHT = 2000;
+	
+	private FactalTree graphic;
+	private JPanel appletPanel;
+	
+	public void init(){
+		graphic = new FractalTree(APPLET_WIDTH,APPLET_HEIGHT);
+		appletPanel = new JPanel();
+		appletPanel.add(graphic);
+		getContentPane().add(appletPanel);
+		setSize(APPLET_WIDTH,APPLET_HEIGHT);
+	}
+}
