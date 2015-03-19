a5e37c5ce4c06fc697b83bfd798ba7a2605bd0a9
Helithumper <peyton.duncan45@gmail.com>
2015-03-19 08:22:14 -0400

    Added Recursive Method

--- src/trees/FractalTree.java
+++ src/trees/FractalTree.java
@@ -2,17 +2,27 @@ package trees;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
 public class FractalTree extends JPanel {
-	
-	
+
 	public FractalTree(int PANEL_WIDTH, int PANEL_HEIGHT) {
 		setBackground(Color.WHITE);
 		setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
 
 	}
-	
-	//Recursive Method
+
+	// Recursive Method
+	private void drawTree(Graphics g, int x1, int y1, double angle, int depth) {
+		if (depth == 0) {
+			return;
+		}
+		int x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * depth * 10.0);
+		int y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) * depth * 10.0);
+		g.drawLine(x1, y1, x2, y2);
+		drawTree(g, x2, y2, angle - 35, depth - 1);
+		drawTree(g, x2, y2, angle + 35, depth - 1);
+	}
 }
